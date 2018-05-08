0141d73788ad1ef6e2259fba94691c4af7943bb5
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2018-05-08 11:38:15 -0300

    Inserindo a rota delete

--- index.php
+++ index.php
@@ -73,6 +73,15 @@ $app->get('/admin/users/create', function(){
 $app->get("/admin/users/:iduser/delete", function($iduser){
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
 });
 
 $app->get('/admin/users/:iduser', function($iduser){
@@ -104,7 +113,23 @@ $app->get("/admin/users/:iduser", function($iduser){
 	User::verifyLogin();
 });
 
+$app->post("/admin/users/:iduser", function($iduser){
 
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+});
 
 
 
