f9d7a3fbff572b77abc6fd68ec89a8c0e1127852
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2018-07-23 14:28:48 -0300

    Realizado o CRUD das categorias

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \RafaelaCampos\Page;
 use \RafaelaCampos\PageAdmin;
 use \RafaelaCampos\Model\User;
+use \RafaelaCampos\Model\Category;
 
 $app = new Slim();
 
@@ -131,7 +132,86 @@ $app->post("/admin/users/:iduser", function($iduser){
 	exit;
 });
 
+$app->get("/admin/categories", function(){
 
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
 
 $app->run();
 
